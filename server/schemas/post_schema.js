bdf7d0b84eacafe2f0a085f4b47a61679525237f
jiachien1206 <nini19961206@gmail.com>
2023-04-13 18:07:03 +0800

    feat: post schema

---
+++ server/schemas/post_schema.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    title: { type: String, required: [true, 'Please fill the title.'], maxLength: 100 },
+    authorId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
+    location: {
+        continent: {
+            type: String,
+            default: null,
+            enum: ['亞洲', '歐洲', '北美洲', '大洋洲', '南美洲', '非洲', '南極洲'],
+        },
+        country: { type: String, default: null },
+        city: { type: String, default: null },
+    },
+    tags: [
+        {
+            type: String,
+            default: null,
+            enum: ['交通', '住宿', '景點', '證件', '其他', '恐怖故事', '省錢妙招'],
+        },
+    ],
+    content: { type: String, required: [true, 'Write some content.'] },
+    dates: {
+        post_date: { type: Date, default: Date.now, immutable: true },
+        travel_date: { type: Date, default: Date.now },
+        last_interact: { type: Date, default: Date.now },
+    },
+    comments: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Comment' }],
+    new_read_num: { type: Number, default: 0 },
+    read_num: { type: Number, default: 0 },
+    new_like_num: { type: Number, default: 0 },
+    like_num: { type: Number, default: 0 },
+    new_save_num: { type: Number, default: 0 },
+    save_num: { type: Number, default: 0 },
+    new_comments_num: { type: Number, default: 0 },
+    comments_num: { type: Number, default: 0 },
+    score: { type: Number, default: 0 },
+});
+
+export default mongoose.model('Post', postSchema);
