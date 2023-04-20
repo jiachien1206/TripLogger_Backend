f3658350398e528051f42804043944dcf2b951d3
jiachien1206 <nini19961206@gmail.com>
2023-04-20 20:54:37 +0800

    fix: consumer can't connect to elasticsearch through environment variable

--- util/elasticsearch.js
+++ util/elasticsearch.js
@@ -1,9 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { URL } from 'url';
 import { Client } from '@elastic/elasticsearch';
-const esHost = process.env.ES_HOST;
-console.log(esHost == 'http://152.67.209.195:9205');
-const client = new Client({ node: esHost });
+const client = new Client({ node: 'http://152.67.209.195:9205' });
 
 export default client;
