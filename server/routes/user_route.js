a615db9a2cd37e6452c251ba5095e7268bb7094d
jiachien1206 <nini19961206@gmail.com>
2023-04-22 20:33:02 +0800

    feat: delete user id in user routes

--- server/routes/user_route.js
+++ server/routes/user_route.js
@@ -16,11 +16,11 @@ import {
 
 router.route('/user/signup').post(wrapAsync(signup));
 router.route('/user/signin').post(wrapAsync(signin));
-router.route('/users/:id/setting').get(authentication, wrapAsync(getUserData));
-router.route('/users/:id/setting').put(authentication, wrapAsync(editUserSetting));
-router.route('/users/:id/posts').get(wrapAsync(getUserPosts));
-router.route('/users/:id/visited').get(authentication, wrapAsync(getUserVisited));
-router.route('/users/:id/saved').post(authentication, wrapAsync(getUserSavedPosts));
+router.route('/user/setting').get(authentication, wrapAsync(getUserData));
+router.route('/user/setting').put(authentication, wrapAsync(editUserSetting));
+router.route('/user/posts').get(authentication, wrapAsync(getUserPosts));
+router.route('/user/visited').get(authentication, wrapAsync(getUserVisited));
+router.route('/user/saved').post(authentication, wrapAsync(getUserSavedPosts));
 router.route('/user/newsfeed').post(authentication, wrapAsync(generateUserNewsfeed));
 
 export default router;
