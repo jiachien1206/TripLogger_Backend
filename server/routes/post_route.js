7e9aca898a694ae71f4a0d9affd343e826deab38
jiachien1206 <nini19961206@gmail.com>
2023-05-21 16:50:39 +0800

    hotfix: /posts/:id API controller add wrap async

--- server/routes/post_route.js
+++ server/routes/post_route.js
@@ -25,7 +25,7 @@ import authentication from '../middlewares/authentication.js';
 router.route('/latest-posts').get(wrapAsync(getNewPosts));
 router.route('/top-posts').get(wrapAsync(getTopPosts));
 router.route('/relevant-posts').get(authentication, wrapAsync(getRelevantPosts));
-router.route('/posts/:id').get(getPost);
+router.route('/posts/:id').get(wrapAsync(getPost));
 router.route('/posts/:id/read').post(wrapAsync(readPost));
 router.route('/posts/:id/like').post(authentication, wrapAsync(likePost));
 router.route('/posts/:id/save').post(authentication, wrapAsync(savePost));
