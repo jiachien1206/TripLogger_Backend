a9d55abe92b0baa2a5b9eafc57931a6e41b539c6
jiachien1206 <nini19961206@gmail.com>
2023-05-21 16:50:24 +0800

    hotfix: try catch authentication middleware

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 const authentication = async (req, res, next) => {
-    let accessToken = req.get('Authorization');
-
-    if (!accessToken) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-    accessToken = accessToken.replace('Bearer ', '');
     try {
+        let accessToken = req.get('Authorization');
+
+        if (!accessToken) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        accessToken = accessToken.replace('Bearer ', '');
         const user = await util.promisify(jwt.verify)(accessToken, process.env.TOKEN_SECRET);
         req.user = user;
         next();
